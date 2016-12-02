c68a327bc5b0b55696915cc1676eca3f9260b64c
Francis Benyah <francis.benyah@gmail.com>
2016-12-02 12:55:43 +0200

    add application context

---
+++ StructuredIT.DAL/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using StructuredIT.Models.User;
+
+namespace StructuredIT.DAL
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("ApplicationContext", false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        #region Properties
+            //public virtual DbSet<Models.Media.Genre> Genres { get; set; }
+            //public virtual DbSet<Models.Media.MediaItem> MediaItems { get; set; }
+        #endregion
+    }
+}
