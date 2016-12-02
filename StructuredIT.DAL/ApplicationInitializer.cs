22a7c6622b53a55d5af2789510fa73ddc5d7e72d
Francis Benyah <francis.benyah@gmail.com>
2016-12-02 12:56:36 +0200

    add application inintializer

---
+++ StructuredIT.DAL/ApplicationInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuredIT.DAL
+{
+    public class ApplicationInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            
+        }
+    }
+}
