b5d60eed1fecd2599f04fbe1e72facf80ee83b11
Francis Benyah <francis.benyah@gmail.com>
2016-12-02 13:19:58 +0200

    added artist model

---
+++ StructuredIT.Models/Core/Artist.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuredIT.Models.Core
+{
+    public class Artist
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string UniqueIdentifier { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(3)]
+        public string CountryCode { get; set; }
+        
+        public string Aliases { get; set; }
+    }
+}
